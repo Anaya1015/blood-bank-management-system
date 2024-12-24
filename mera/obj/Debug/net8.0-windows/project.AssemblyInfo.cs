798f565765322ae2bac83352e963c49037b41690
Kainat Khurram <kainat.khuram786@gmail.com>
2024-12-24 15:21:25 -0800

    recipient dashoboard working

--- mera/obj/Debug/net8.0-windows/project.AssemblyInfo.cs
+++ mera/obj/Debug/net8.0-windows/project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4184f5f408bcfcd2f05f742bb81887a477b7d36e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49198f1dd21da4b2858f07c1f8e540612f2fdbfb")]
 [assembly: System.Reflection.AssemblyProductAttribute("project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
